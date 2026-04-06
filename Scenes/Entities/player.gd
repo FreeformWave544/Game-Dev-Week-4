a850fc9fb870a6440badc1da8815a5a7ae3bc9ee
FreeformWave544 <165945352+FreeformWave544@users.noreply.github.com>
2026-04-06 20:41:03 +0100

    3 HOURS FOR ONE BUG

--- Scenes/Entities/player.gd
+++ Scenes/Entities/player.gd
@@ -3,12 +3,39 @@ class_name Player
 
 const SPEED := 300.0
 const JUMP_VELOCITY := -400.0
-var score := 0
+var salad = []
+var saladCount := 0
 
 func _physics_process(_delta: float) -> void:
 	var direction := Input.get_vector("left", "right", "up", "down")
-	if direction.x: velocity.x = direction.x * SPEED
-	else: velocity.x = move_toward(velocity.x, 0, SPEED)
-	if direction.y: velocity.y = direction.y * SPEED
-	else: velocity.y = move_toward(velocity.y, 0, SPEED)
+	velocity = direction * SPEED
 	move_and_slide()
+
+func _update_UI() -> void:
+	var existingSaladCount = len($UI.get_children())
+	for i in range(len(salad)):
+		if i >= existingSaladCount:
+			var newSalad = $Salad.duplicate(0)
+			$UI.add_child(newSalad)
+			newSalad.frame = salad[i]
+			newSalad.show()
+		var currentSalad = $UI.get_child(i)
+		currentSalad.global_position.x = (128 * currentSalad.scale.x) * i - (128 * (currentSalad.scale.x - 1) / 2)
+	#var saladName = ""
+	#match salad[i]:
+		#0: saladName = "Shredded Cabbage"
+		#1: saladName = "Vegetable Dressing Salad"
+		#2: saladName = "Ham and Cucumber Salad"
+		#3: saladName = "Potato Salad"
+		#4: saladName = "Avocado Salad"
+		#5: saladName = "Pumpkin Salad"
+		#6: saladName = "Carrot Shirishiri"
+		#7: saladName = "Fruit Salad"
+		#8: saladName = "Onion Salad"
+		#9: saladName = "Fancy Salad"
+		#10: saladName = "Shrimp Salad"
+		#11: saladName = "Egg Salad"
+		#12: saladName = "Kochori Salad"
+		#13: saladName = "Caesar Salad"
+		#14: saladName = "Tomato Salad"
+		#15: saladName = "Simple Salad"
--- Scenes/collectable.gd
+++ Scenes/collectable.gd
@@ -1,18 +1,32 @@
-extends Area2D
+extends StaticBody2D
 
+var interactable := false
 @onready var particle_system: GPUParticles2D = $GPUParticles2D
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
-		body.score += 1
-		$CollisionShape2D.set_deferred("disabled", true)
-		for i in range(200):
-			$GPUParticles2D.fixed_fps = randi_range(1, 30)
+		interactable = true
+		while interactable and not Input.is_action_pressed("interact"):
 			await get_tree().process_frame
-		await get_tree().create_timer(0.1).timeout
-		var player_position = get_parent().find_child("Player").global_position
-		$GPUParticles2D.process_material.gravity = Vector3(player_position.x, player_position.y, 0.0)
-		$GPUParticles2D.fixed_fps = 30
-		$GPUParticles2D.speed_scale = 3.0
-		$GPUParticles2D.one_shot = true
-		await get_tree().create_timer(8.4).timeout
-		modulate = Color(0.5, 0.7, 0.5, 0.3)
+		if Input.is_action_just_pressed("interact"):
+			$CollectionArea/CollisionShape2D.set_deferred("disabled", true)
+			for i in range(200):
+				$GPUParticles2D.fixed_fps = randi_range(1, 30)
+				await get_tree().process_frame
+			await get_tree().create_timer(0.1).timeout
+			var player_position = get_parent().find_child("Player").global_position
+			$GPUParticles2D.process_material.gravity = Vector3(player_position.x, player_position.y, 0.0)
+			$GPUParticles2D.fixed_fps = 30
+			$GPUParticles2D.speed_scale = 3.0
+			$GPUParticles2D.one_shot = true
+			await get_tree().create_timer(8.4).timeout
+			modulate = Color(0.5, 0.7, 0.5, 0.3)
+			await get_tree().create_timer(0.2).timeout
+			$Salads.show()
+			await get_tree().process_frame
+			for i in range(50):
+				$Salads.frame = randi_range(0, 15)
+				await get_tree().process_frame
+			body.salad.append(int($Salads.frame))
+			body._update_UI()
+
+func _on_collection_area_body_exited(_body: Node2D) -> void: interactable = false
